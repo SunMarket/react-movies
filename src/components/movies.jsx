d39989b1bf0d74ccae2c828b33b8dd4a56bc91fa
SunMarket <77450898+SunMarket@users.noreply.github.com>
2022-12-05 12:36:45 +0300

    project complete

---
+++ src/components/movies.jsx
@@ -0,0 +1,12 @@
+import {Movie} from './movie';
+function Movies(props) {
+    const {movies = []} = props;
+    return <div className="movies">
+        {movies.length ? movies.map(movie => (
+            <Movie key={movie.imdbID} {...movie} />
+        )) : <h4>Ничего не найдено!</h4>
+    }
+    </div>
+}
+
+export {Movies};
